3566c719c2c4ccedade0221736127f21d9751b05
Giwan Kim <giwan.kim@gmail.com>
2024-04-23 10:03:18 +0900

    feat(storage): 데이터베이스 모듈 추가

---
+++ storage/db-core/src/main/java/com/giwankim/taxiservice/storage/db/core/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.giwankim.taxiservice.storage.db.core;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+}
