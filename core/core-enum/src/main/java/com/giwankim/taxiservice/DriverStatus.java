63665fc992569c3f579d484e4d0590d2725de3b5
Giwan Kim <giwan.kim@gmail.com>
2024-05-05 12:20:31 +0900

    feat(core-enum): 도메인, 영속성, API 모듈에서 공통적으로 사용할 수 있는 enum 모듈을 추가했다.

---
+++ core/core-enum/src/main/java/com/giwankim/taxiservice/DriverStatus.java
@@ -0,0 +1,5 @@
+package com.giwankim.taxiservice;
+
+public enum DriverStatus {
+  OFFLINE, AVAILABLE, IN_ROUTE_TO_PASSENGER, TRIP_IN_PROGRESS, TRIP_COMPLETED
+}
---
+++ core/core-enum/src/main/java/com/giwankim/taxiservice/TripStatus.java
@@ -0,0 +1,5 @@
+package com.giwankim.taxiservice;
+
+public enum TripStatus {
+  MATCHED, PICKED_UP, IN_PROGRESS, PAID, DROPPED_OFF, COMPLETED, CANCELLED
+}
