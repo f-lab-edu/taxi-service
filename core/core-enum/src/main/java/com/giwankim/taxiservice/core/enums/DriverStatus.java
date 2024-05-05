92db6bf873761acac8de9a1bed8e061b49e7b895
Giwan Kim <giwan.kim@gmail.com>
2024-05-05 13:25:28 +0900

    fix(core-enum): 내부 패키지 구조를 통일화했다.

--- core/core-enum/src/main/java/com/giwankim/taxiservice/core/enums/DriverStatus.java
+++ core/core-enum/src/main/java/com/giwankim/taxiservice/core/enums/DriverStatus.java
@@ -1,4 +1,4 @@
-package com.giwankim.taxiservice;
+package com.giwankim.taxiservice.core.enums;
 
 public enum DriverStatus {
   OFFLINE, AVAILABLE, IN_ROUTE_TO_PASSENGER, TRIP_IN_PROGRESS, TRIP_COMPLETED
--- core/core-enum/src/main/java/com/giwankim/taxiservice/core/enums/TripStatus.java
+++ core/core-enum/src/main/java/com/giwankim/taxiservice/core/enums/TripStatus.java
@@ -1,4 +1,4 @@
-package com.giwankim.taxiservice;
+package com.giwankim.taxiservice.core.enums;
 
 public enum TripStatus {
   MATCHED, PICKED_UP, IN_PROGRESS, PAID, DROPPED_OFF, COMPLETED, CANCELLED
