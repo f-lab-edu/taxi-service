1f7bc796a48d877c0653d233458ed610224f514d
Giwan Kim <giwan.kim@gmail.com>
2024-05-05 16:22:44 +0900

    feat(domain): 검색어를 통해 위치를 서치할 수 있는 유즈 케이스를 추가한다.

--- core/core-domain/src/main/java/com/giwankim/taxiservice/core/domain/application/port/in/SearchLocationsUseCase.java
+++ core/core-domain/src/main/java/com/giwankim/taxiservice/core/domain/application/port/in/SearchLocationsUseCase.java
@@ -1,12 +1,6 @@
 package com.giwankim.taxiservice.core.domain.application.port.in;
 
-import com.giwankim.taxiservice.common.SelfValidating;
 import com.giwankim.taxiservice.core.domain.domain.Location;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import lombok.EqualsAndHashCode;
-import lombok.Value;
 
 import java.util.List;
 
@@ -14,27 +8,4 @@ public interface SearchLocationsUseCase {
 
   List<Location> searchLocations(SearchLocationsQuery query);
 
-  @Value
-  @EqualsAndHashCode(callSuper = false)
-  class SearchLocationsQuery extends SelfValidating<SearchLocationsQuery> {
-    @NotEmpty
-    String keyword;
-
-    @Min(1)
-    @Max(45)
-    Integer page;
-
-    @Min(1)
-    @Max(30)
-    Integer size;
-
-    public SearchLocationsQuery(String keyword,
-                                Integer page,
-                                Integer size) {
-      this.keyword = keyword;
-      this.page = page == null ? 1 : page;
-      this.size = size == null ? 10 : size;
-      this.validateSelf();
-    }
-  }
 }
