460620db43856e67b04b8a15d33b144c6a276f36
Giwan Kim <giwan.kim@gmail.com>
2024-05-08 12:43:49 +0900

    fix(common): common 모듈을 삭제한다.

--- core/core-domain/src/main/java/com/giwankim/taxiservice/core/domain/application/service/SearchLocationsService.java
+++ core/core-domain/src/main/java/com/giwankim/taxiservice/core/domain/application/service/SearchLocationsService.java
@@ -1,16 +1,15 @@
 package com.giwankim.taxiservice.core.domain.application.service;
 
-import com.giwankim.taxiservice.common.UseCase;
-import com.giwankim.taxiservice.core.domain.application.port.in.SearchLocationsQuery;
 import com.giwankim.taxiservice.core.domain.application.port.in.SearchLocationsUseCase;
 import com.giwankim.taxiservice.core.domain.application.port.out.SearchLocationPort;
 import com.giwankim.taxiservice.core.domain.domain.Location;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@UseCase
+@Component
 public class SearchLocationsService implements SearchLocationsUseCase {
 
   private final SearchLocationPort searchLocationPort;
