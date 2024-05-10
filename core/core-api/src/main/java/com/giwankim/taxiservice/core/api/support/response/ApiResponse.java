37a13a0007eeede05ab3f76bd8c48dfe85fe4fb8
Giwan Kim <giwan.kim@gmail.com>
2024-05-10 14:53:25 +0900

    feat(api): API 응답값 클래스를 추가한다.

---
+++ core/core-api/src/main/java/com/giwankim/taxiservice/core/api/support/response/ApiResponse.java
@@ -0,0 +1,30 @@
+package com.giwankim.taxiservice.core.api.support.response;
+
+import com.giwankim.taxiservice.core.api.support.error.ErrorMessage;
+import com.giwankim.taxiservice.core.api.support.error.ErrorType;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ApiResponse<T> {
+  private final ResultType result;
+  private final T data;
+  private final ErrorMessage error;
+
+  public static ApiResponse<?> success() {
+    return new ApiResponse<>(ResultType.SUCCESS, null, null);
+  }
+
+  public static <T> ApiResponse<T> success(T data) {
+    return new ApiResponse<>(ResultType.SUCCESS, data, null);
+  }
+
+  public static ApiResponse<?> failure(ErrorType errorType) {
+    return new ApiResponse<>(ResultType.FAILURE, null, new ErrorMessage(errorType));
+  }
+
+  public static ApiResponse<?> failure(ErrorType errorType, Object errorData) {
+    return new ApiResponse<>(ResultType.FAILURE, errorData, new ErrorMessage(errorType, errorData));
+  }
+}
---
+++ core/core-api/src/main/java/com/giwankim/taxiservice/core/api/support/response/ResultType.java
@@ -0,0 +1,5 @@
+package com.giwankim.taxiservice.core.api.support.response;
+
+public enum ResultType {
+  SUCCESS, FAILURE
+}
