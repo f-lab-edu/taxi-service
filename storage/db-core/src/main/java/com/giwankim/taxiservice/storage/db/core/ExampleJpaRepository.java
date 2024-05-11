34821f4f6cf87b7971ddcc78c1c819e59f0da1cd
Giwan Kim <giwan.kim@gmail.com>
2024-05-11 11:20:31 +0900

    feat(db): 로컬 개발용 데이터베이스를 H2로 변경한다.

---
+++ storage/db-core/src/main/java/com/giwankim/taxiservice/storage/db/core/ExampleJpaRepository.java
@@ -0,0 +1,6 @@
+package com.giwankim.taxiservice.storage.db.core;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExampleJpaRepository extends JpaRepository<ExampleJpaEntity, Long> {
+}
