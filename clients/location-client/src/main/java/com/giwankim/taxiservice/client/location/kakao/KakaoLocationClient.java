83eeb1029af7a832d52b3c7f053084aa2c42f212
Giwan Kim <giwan.kim@gmail.com>
2024-05-26 10:46:22 +0900

    refactor(client): mapper 클래스를 제거한다.

--- clients/location-client/src/main/java/com/giwankim/taxiservice/client/location/kakao/KakaoLocationClient.java
+++ clients/location-client/src/main/java/com/giwankim/taxiservice/client/location/kakao/KakaoLocationClient.java
@@ -2,25 +2,20 @@ package com.giwankim.taxiservice.client.location.kakao;
 
 import com.giwankim.taxiservice.core.domain.application.port.out.SearchLocationPort;
 import com.giwankim.taxiservice.core.domain.domain.Location;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Component
 class KakaoLocationClient implements SearchLocationPort {
 
   private final KakaoLocationApi locationApi;
-  private final KakaoLocationMapper locationMapper;
-
-  public KakaoLocationClient(KakaoLocationApi locationApi, KakaoLocationMapper locationMapper) {
-    this.locationApi = locationApi;
-    this.locationMapper = locationMapper;
-  }
 
   @Override
   public List<Location> searchLocations(String keyword, int page, int size) {
     KakaoLocationRequest request = new KakaoLocationRequest(keyword, page, size);
-    KakaoLocationResponse response = locationApi.searchLocations(request);
-    return locationMapper.mapToLocations(response);
+    return locationApi.searchLocations(request).toLocations();
   }
 }
--- clients/location-client/src/main/java/com/giwankim/taxiservice/client/location/kakao/KakaoLocationResponse.java
+++ clients/location-client/src/main/java/com/giwankim/taxiservice/client/location/kakao/KakaoLocationResponse.java
@@ -3,12 +3,24 @@ package com.giwankim.taxiservice.client.location.kakao;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.giwankim.taxiservice.core.domain.domain.Location;
 
 import java.util.List;
 
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
 record KakaoLocationResponse(Meta meta, List<Document> documents) {
+
+  List<Location> toLocations() {
+    return documents()
+        .stream()
+        .map(document ->
+            new Location(
+                document.addressName(),
+                document.y(),
+                document.x()))
+        .toList();
+  }
 
   @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
   @JsonIgnoreProperties(ignoreUnknown = true)
