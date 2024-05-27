be19e0bdb8a8996425440c73a8381af5791d2417
Giwan Kim <giwan.kim@gmail.com>
2024-05-27 17:04:39 +0900

    fix(client): test application에서 datasource를 찾는 작업을 방지한다.

--- clients/location-client/src/test/java/com/giwankim/taxiservice/client/ClientTestApplication.java
+++ clients/location-client/src/test/java/com/giwankim/taxiservice/client/ClientTestApplication.java
@@ -2,10 +2,11 @@ package com.giwankim.taxiservice.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @ConfigurationPropertiesScan
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ClientTestApplication {
 
   public static void main(String[] args) {
