451f50a5c911182b258eb3d06380bc1b6f1ee344
Giwan Kim <giwan.kim@gmail.com>
2024-06-06 13:02:33 +0900

    feat(client): 길찾기 API 조회 클라이언트를 구현했다.

---
+++ clients/directions-client/src/test/java/com/giwankim/taxiservice/client/directions/FileUtils.java
@@ -0,0 +1,19 @@
+package com.giwankim.taxiservice.client.directions;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import org.springframework.util.ResourceUtils;
+
+public class FileUtils {
+  private FileUtils() {}
+
+  public static String read(String filePath) {
+    try {
+      File file = ResourceUtils.getFile(filePath);
+      return new String(Files.readAllBytes(file.toPath()));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
