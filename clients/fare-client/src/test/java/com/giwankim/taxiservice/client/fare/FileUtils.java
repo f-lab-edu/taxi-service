e5fcbf7768813eeda09785b7fadc0883aa5c2049
Giwan Kim <giwan.kim@gmail.com>
2024-06-09 16:15:07 +0900

    fix(client): `FileUtils.read()` 한줄씩 파일을 읽어드리도록 구현한다.

--- clients/fare-client/src/test/java/com/giwankim/taxiservice/client/fare/FileUtils.java
+++ clients/fare-client/src/test/java/com/giwankim/taxiservice/client/fare/FileUtils.java
@@ -1,9 +1,11 @@
 package com.giwankim.taxiservice.client.fare;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import org.springframework.util.ResourceUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileUtils {
   private FileUtils() {}
@@ -11,7 +13,14 @@ public class FileUtils {
   public static String read(String filePath) {
     try {
       File file = ResourceUtils.getFile(filePath);
-      return new String(Files.readAllBytes(file.toPath()));
+      StringBuilder stringBuilder = new StringBuilder();
+      try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+        String line;
+        while ((line = reader.readLine()) != null) {
+          stringBuilder.append(line).append("\n");
+        }
+      }
+      return stringBuilder.toString();
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
