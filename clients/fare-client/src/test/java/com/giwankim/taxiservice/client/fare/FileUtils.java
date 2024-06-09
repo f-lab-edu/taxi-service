41676ab6b945107ad9ded585984be22590b9692f
Giwan Kim <giwan.kim@gmail.com>
2024-06-09 18:39:57 +0900

    fix(directions-client): 스트림을 활용해서 더 간결하게 파일을 한줄씩 읽는다.

--- clients/fare-client/src/test/java/com/giwankim/taxiservice/client/fare/FileUtils.java
+++ clients/fare-client/src/test/java/com/giwankim/taxiservice/client/fare/FileUtils.java
@@ -1,11 +1,11 @@
 package com.giwankim.taxiservice.client.fare;
 
-import org.springframework.util.ResourceUtils;
-
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.stream.Collectors;
+import org.springframework.util.ResourceUtils;
 
 public class FileUtils {
   private FileUtils() {}
@@ -13,14 +13,9 @@ public class FileUtils {
   public static String read(String filePath) {
     try {
       File file = ResourceUtils.getFile(filePath);
-      StringBuilder stringBuilder = new StringBuilder();
-      try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-        String line;
-        while ((line = reader.readLine()) != null) {
-          stringBuilder.append(line).append("\n");
-        }
+      try (BufferedReader reader = Files.newBufferedReader(file.toPath())) {
+        return reader.lines().collect(Collectors.joining(System.lineSeparator()));
       }
-      return stringBuilder.toString();
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
