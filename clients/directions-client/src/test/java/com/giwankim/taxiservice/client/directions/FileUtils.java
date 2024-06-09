0b62f681e03ef6daea049a949fe96f413198107b
Giwan Kim <giwan.kim@gmail.com>
2024-06-09 17:33:20 +0900

    fix(directions-client): 파일을 한줄씩 읽는다.

--- clients/directions-client/src/test/java/com/giwankim/taxiservice/client/directions/FileUtils.java
+++ clients/directions-client/src/test/java/com/giwankim/taxiservice/client/directions/FileUtils.java
@@ -1,5 +1,6 @@
 package com.giwankim.taxiservice.client.directions;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,7 +12,11 @@ public class FileUtils {
   public static String read(String filePath) {
     try {
       File file = ResourceUtils.getFile(filePath);
-      return new String(Files.readAllBytes(file.toPath()));
+      try (BufferedReader reader = Files.newBufferedReader(file.toPath())) {
+        StringBuilder stringBuilder = new StringBuilder();
+        reader.lines().forEach(line -> stringBuilder.append(line).append("\n"));
+        return stringBuilder.toString();
+      }
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
