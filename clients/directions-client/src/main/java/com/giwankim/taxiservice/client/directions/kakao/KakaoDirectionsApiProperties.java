4a8fcb1c5be833ed9c991744325c5fdf1c5dd264
Giwan Kim <giwan.kim@gmail.com>
2024-06-11 14:21:12 +0900

    test(directions-client): 추가 설정을 위한 빈을 추가한다.

--- clients/directions-client/src/main/java/com/giwankim/taxiservice/client/directions/kakao/KakaoDirectionsApiProperties.java
+++ clients/directions-client/src/main/java/com/giwankim/taxiservice/client/directions/kakao/KakaoDirectionsApiProperties.java
@@ -1,25 +1,18 @@
 package com.giwankim.taxiservice.client.directions.kakao;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.validator.constraints.URL;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
 @Configuration(proxyBeanMethods = false)
-@ConfigurationProperties(prefix = "directions.kakao")
+@ConfigurationProperties(prefix = "client.config.kakao-directions-api")
 @Getter
 @Setter
-public class KakaoDirectionsConfigProperties {
-  private Api api;
-
-  @Getter
-  @Setter
-  @Validated
-  public static class Api {
-    @NotEmpty private String key;
-    @URL private String url;
-  }
+@Validated
+public class KakaoDirectionsApiProperties {
+  @PositiveOrZero private Integer connectTimeout;
+  @PositiveOrZero private Integer readTimeout;
 }
