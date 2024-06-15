b7fa5f382f98e422232ccfc3de22b46b7937f3f8
Giwan Kim <giwan.kim@gmail.com>
2024-06-15 12:10:48 +0900

    feat(domain): 예상 요금 서비스 클래스를 추가한다.

---
+++ core/core-domain/src/main/java/com/giwankim/taxiservice/core/domain/application/service/GetTripEstimatesService.java
@@ -0,0 +1,37 @@
+package com.giwankim.taxiservice.core.domain.application.service;
+
+import com.giwankim.taxiservice.core.domain.application.port.in.GetTripEstimatesQuery;
+import com.giwankim.taxiservice.core.domain.application.port.in.GetTripEstimatesUseCase;
+import com.giwankim.taxiservice.core.domain.application.port.out.LoadBaseFarePort;
+import com.giwankim.taxiservice.core.domain.application.port.out.LoadDirectionsPort;
+import com.giwankim.taxiservice.core.domain.domain.Directions;
+import com.giwankim.taxiservice.core.domain.domain.Money;
+import com.giwankim.taxiservice.core.domain.domain.TripEstimate;
+import com.giwankim.taxiservice.core.domain.domain.TripEstimates;
+import com.giwankim.taxiservice.core.enums.TaxiType;
+import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class GetTripEstimatesService implements GetTripEstimatesUseCase {
+
+  private final LoadDirectionsPort loadDirectionsPort;
+  private final LoadBaseFarePort loadBaseFarePort;
+
+  @Override
+  public TripEstimates getTripEstimates(GetTripEstimatesQuery query) {
+    Directions directions = loadDirectionsPort.loadDirections(query.origin(), query.destination());
+
+    Money baseFare = loadBaseFarePort.loadBaseFare(query.origin(), query.destination());
+
+    List<TripEstimate> estimates =
+        Arrays.stream(TaxiType.values())
+            .map(taxiType -> TripEstimate.create(taxiType, baseFare))
+            .toList();
+
+    return new TripEstimates(baseFare, directions, estimates);
+  }
+}
