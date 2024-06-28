20d4810adf9d3dabc6180e5f001b322f9a794b84
Giwan Kim <giwan.kim@gmail.com>
2024-06-28 15:38:34 +0900

    feat(api): 전화번호를 마스킹 구현체를 추가한다.

--- core/core-api/src/test/java/com/giwankim/taxiservice/core/api/support/masking/NameMaskerTest.java
+++ core/core-api/src/test/java/com/giwankim/taxiservice/core/api/support/masking/NameMaskerTest.java
@@ -2,6 +2,7 @@ package com.giwankim.taxiservice.core.api.support.masking;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -17,28 +18,40 @@ class NameMaskerTest {
 
   @Test
   void should_mask_three_letter_korean_name() {
-    assertThat(masker.mask("강감찬")).isEqualTo("강*찬");
-    assertThat(masker.mask("김유신")).isEqualTo("김*신");
-    assertThat(masker.mask("이순신")).isEqualTo("이*신");
+    SoftAssertions.assertSoftly(
+        softly -> {
+          softly.assertThat(masker.mask("강감찬")).isEqualTo("강*찬");
+          softly.assertThat(masker.mask("김유신")).isEqualTo("김*신");
+          softly.assertThat(masker.mask("이순신")).isEqualTo("이*신");
+        });
   }
 
   @Test
   void should_mask_four_letter_korean_name() {
-    assertThat(masker.mask("남궁건우")).isEqualTo("남**우");
-    assertThat(masker.mask("독고영재")).isEqualTo("독**재");
+    SoftAssertions.assertSoftly(
+        softly -> {
+          softly.assertThat(masker.mask("남궁건우")).isEqualTo("남**우");
+          softly.assertThat(masker.mask("독고영재")).isEqualTo("독**재");
+        });
   }
 
   @Test
   void should_mask_an_English_name_with_less_than_6_characters() {
-    assertThat(masker.mask("Ty")).isEqualTo("Ty");
-    assertThat(masker.mask("Sam")).isEqualTo("S*m");
-    assertThat(masker.mask("Jane")).isEqualTo("J**e");
-    assertThat(masker.mask("Susan")).isEqualTo("S***n");
+    SoftAssertions.assertSoftly(
+        softly -> {
+          softly.assertThat(masker.mask("Ty")).isEqualTo("Ty");
+          softly.assertThat(masker.mask("Sam")).isEqualTo("S*m");
+          softly.assertThat(masker.mask("Jane")).isEqualTo("J**e");
+          softly.assertThat(masker.mask("Susan")).isEqualTo("S***n");
+        });
   }
 
   @Test
   void should_properly_mask_an_English_name_with_6_or_more_characters() {
-    assertThat(masker.mask("David Beckham")).isEqualTo("Da*** *****am");
-    assertThat(masker.mask("Luke Skywalker")).isEqualTo("Lu** *******er");
+    SoftAssertions.assertSoftly(
+        softly -> {
+          softly.assertThat(masker.mask("David Beckham")).isEqualTo("Da*** *****am");
+          softly.assertThat(masker.mask("Luke Skywalker")).isEqualTo("Lu** *******er");
+        });
   }
 }
