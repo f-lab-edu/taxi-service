4e4f664e32c84687653e8dafa068dfabb72b86f1
Giwan Kim <giwan.kim@gmail.com>
2024-06-30 11:39:17 +0900

    feat(domain): 기사님 도메인 모델에 출/퇴근 상태를 추가한다.

--- core/core-domain/src/main/java/com/giwankim/taxiservice/core/domain/domain/Driver.java
+++ core/core-domain/src/main/java/com/giwankim/taxiservice/core/domain/domain/Driver.java
@@ -1,12 +1,14 @@
 package com.giwankim.taxiservice.core.domain.domain;
 
 import java.net.URL;
+
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class Driver {
   private final DriverId id;
+  private final DriverStatus status;
   private final Vehicle vehicle;
   private final String name;
   private final String phoneNumber;
@@ -16,12 +18,14 @@ public class Driver {
   @Builder
   public Driver(
       DriverId id,
+      DriverStatus status,
       Vehicle vehicle,
       String name,
       String phoneNumber,
       URL profilePicture,
       Location location) {
     this.id = id;
+    this.status = status;
     this.vehicle = vehicle;
     this.name = name;
     this.phoneNumber = phoneNumber;
@@ -29,5 +33,6 @@ public class Driver {
     this.location = location;
   }
 
-  public record DriverId(Long value) {}
+  public record DriverId(Long value) {
+  }
 }
