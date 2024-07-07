79cef7e3dc5aba17949692d1fec775e787db326d
Giwan Kim <giwan.kim@gmail.com>
2024-07-07 12:18:39 +0900

    feat(domain): `PickupRequest` 관련 out-por signature 수정

--- core/core-domain/src/main/java/com/giwankim/taxiservice/core/domain/application/port/out/UpdatePickupRequestStatePort.java
+++ core/core-domain/src/main/java/com/giwankim/taxiservice/core/domain/application/port/out/UpdatePickupRequestStatePort.java
@@ -2,6 +2,6 @@ package com.giwankim.taxiservice.core.domain.application.port.out;
 
 import com.giwankim.taxiservice.core.domain.domain.PickupRequest;
 
-public interface StorePickupRequestPort {
-  PickupRequest storePickupRequest(PickupRequest pickupRequest, Long ttlInSeconds);
+public interface UpdatePickupRequestStatePort {
+  void updatePickupRequestState(PickupRequest pickupRequest);
 }
